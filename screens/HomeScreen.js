51d8919ea6de4593c6cf6a314b16e607cd785314
acculief <yajousaki@gmail.com>
2019-12-21 21:51:11 +0900

    Add animated header Example

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,33 +3,18 @@ import { StyleSheet, View, Text } from "react-native";
 import Layout from "../components/Layout";
 import TodoList from "../components/TodoList";
 import AddTodoForm from "../components/AddTodoList";
-import * as AppAuth from "expo-app-auth";
-
 import { useInputValue, useTodos } from "../hooks/todolist";
 
 const HomeScreen = memo(props => {
   const { inputValue, changeInput, clearInput } = useInputValue();
   const { todos, addTodo, checkTodo, removeTodo } = useTodos();
-  const config = {
-    issuer: "https://accounts.google.com",
-    clientId:
-      "713570698875-82bp550ojqpi1fvf5dmrnelp05dhn8n8.apps.googleusercontent.com",
-    scopes: ["profile"]
-  };
   const clearInputAndAddTodo = _ => {
     clearInput();
     addTodo(inputValue);
   };
 
-  const tokenResponse = async () => {
-    const appState = await AppAuth.authAsync(config);
-    console.log(appState);
-    return appState;
-  };
-
   return (
     <Layout>
-      <Text onPress={() => tokenResponse()}>hoge</Text>
       <AddTodoForm
         inputValue={inputValue}
         changeInput={changeInput}
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,132 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React, { Component } from "react";
+import {
+  Animated,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+export default class ScrollableHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: new Animated.Value(0)
+    };
+  }
+
+  _renderScrollViewContent() {
+    const data = Array.from({ length: 30 });
+    return (
+      <View style={styles.scrollViewContent}>
+        {data.map((_, i) => (
+          <View key={i} style={styles.row}>
+            <Text>{i}</Text>
+          </View>
+        ))}
+      </View>
+    );
+  }
+
+  render() {
+    const imageOpacity = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      outputRange: [1, 1, 0],
+      extrapolate: "clamp"
+    });
+    const imageTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -50],
+      extrapolate: "clamp"
+    });
+
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: "clamp"
+    });
+    return (
+      <View style={styles.fill}>
+        <ScrollView
+          style={styles.fill}
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            { nativeEvent: { contentOffset: { y: this.state.scrollY } } }
+          ])}
+        >
+          {this._renderScrollViewContent()}
+        </ScrollView>
+        <Animated.View style={[styles.header, { height: headerHeight }]}>
+          <Animated.Image
+            style={[
+              styles.backgroundImage,
+              {
+                opacity: imageOpacity,
+                transform: [{ translateY: imageTranslate }]
+              }
+            ]}
+            source={{
+              uri:
+                "https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/s79c705b1278cf944/image/i641eb61f22fee191/version/1553241673/image.jpg"
+            }}
+          />
+          <Animated.View>
+            <Text style={styles.title}>Nyaaa</Text>
+          </Animated.View>
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  fill: {
+    flex: 1
   },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: "#D3D3D3",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#03A9F4",
+    overflow: "hidden"
+  },
+  bar: {
+    marginTop: 28,
+    height: 32,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    backgroundColor: "transparent",
+    color: "white",
+    fontSize: 18,
+    alignSelf: "center",
+    marginTop: 30
+  },
+  scrollViewContent: {
+    marginTop: HEADER_MAX_HEIGHT
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    width: null,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: "cover"
+  }
 });
